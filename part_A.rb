2ec567151eea85a4f4a047681d9c0d51ec31350f
Chris Woodall <christopherhwoodall@gmail.com>
2017-04-17 17:53:44 +0100

    ADD Part A

--- part_A.rb
+++ part_A.rb
@@ -1,9 +1,39 @@
 
-class Student
+class CodeClanStudent
 
- def initialize(name,cohort)
-   @name = name
-   @cohort = age
+ def initialize(student_name,cohort)
+   @student_name = student_name
+   @cohort = cohort
+   
+
  end
+
+def get_name 
+  return @student_name
+end
+
+def get_cohort
+  return @cohort
+end
+
+def set_name(new_name)
+   @student_name = new_name
+end
+
+def set_cohort(new_cohort)
+   @cohort = new_cohort
+end
+
+def talk(phrase)
+   return phrase
+end
+
+def fav_languages(language)
+ return " I love #{language} "
+end
+end
+
+
+class Team
 
 end
--- part_A_spec.rb
+++ part_A_spec.rb
@@ -5,19 +5,54 @@ require_relative './part_A'
 class TestCodeClanStudent < MiniTest::Test
 
   def setup 
-    # @student1 = CodeClanStudent.new("Garry", 12)
-    # @student2 = CodeClanStudent.new("Brian", 12)
+    @student1 = CodeClanStudent.new("Garry", 12)
+    @student2 = CodeClanStudent.new("Brian", 12)
+
+    @team = Team.new("")
   end
 
-  def class_creation
+  def test_class_creation
     # want to create a test that demonstrates whether a class has been created or no
-      student = CodeClanStudent.new()
+      student = CodeClanStudent.new("name", 12)
+      refute_nil(student)
   end
 
+#   # getteer methods
+ def test_student_name
+    assert_equal("Garry", @student1.get_name)
+end
 
+def test_student_cohort
+  assert_equal(12, @student2.get_cohort)
+end
 
-  # def test_student
-  #   student_account = CodeClanStudent.new()
-  # end
+def test_set_student_name
+    @student2.set_name("Johnny")
+    assert_equal("Johnny", @student2.get_name)
+end
+
+def test_set_cohort
+    @student1.set_cohort(9)
+    assert_equal(9, @student1.get_cohort)
+end
+
+def test_student_talk
+   assert(@student1.talk("I can talk").is_a?(String))
+end
+
+def test_student_talk
+  assert_equal("I can talk", @student1.talk("I can talk"))  
+end
+
+def test_favorite_languange
+  language = "Ruby"
+  assert_equal((" I love Ruby "), @student1.fav_languages(language))
+end
+
+def test_class_creation
+  # want to create a test that demonstrates whether a class has been created or no
+    team = CodeClanStudent.new("name", 12)
+    refute_nil(student)
+
 
 end
